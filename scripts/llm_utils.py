b9ef3208d5e625f6e3b40c183993f17633e30f3e
Ruben Hohndorf <ruben.hohndorf@gmail.com>
2023-05-06 11:07:32 +0200

    Fix bug in create_chat_completion and env template

--- scripts/llm_utils.py
+++ scripts/llm_utils.py
@@ -4,6 +4,6 @@ from llama_cpp import Llama
 cfg = Config()
 llm = Llama(model_path=cfg.smart_llm_model, n_ctx=2048, embedding=True)
 
-def create_chat_completion(messages[0]["content"], model=None, temperature=cfg.temperature, max_tokens=0)->str:
-    response = llm(messages, stop=["Q:", "### Human:"], echo=False, temperature=temperature, max_tokens=max_tokens)
+def create_chat_completion(messages, model=None, temperature=cfg.temperature, max_tokens=0)->str:
+    response = llm(messages[0]["content"], stop=["Q:", "### Human:"], echo=False, temperature=temperature, max_tokens=max_tokens)
     return response["choices"][0]["text"]
