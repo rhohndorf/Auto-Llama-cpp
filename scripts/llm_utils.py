4cd3e7ab505a190abeb2a2a0ec1b91107b59d414
Ruben Hohndorf <ruben.hohndorf@gmail.com>
2023-05-20 04:24:14 +0200

    Join all messsages to create prompt

--- scripts/llm_utils.py
+++ scripts/llm_utils.py
@@ -4,6 +4,10 @@ from llama_cpp import Llama
 cfg = Config()
 llm = Llama(model_path=cfg.smart_llm_model, n_ctx=2048, embedding=True)
 
+
+
 def create_chat_completion(messages, model=None, temperature=cfg.temperature, max_tokens=0)->str:
-    response = llm(messages[0]["content"], stop=["Q:", "### Human:"], echo=False, temperature=temperature, max_tokens=max_tokens)
+    query = "".join([msg.get("content") for msg in messages])
+    print(query)
+    response = llm(query, stop=["Q:", "### Human:"], echo=False, temperature=temperature, max_tokens=max_tokens)
     return response["choices"][0]["text"]
